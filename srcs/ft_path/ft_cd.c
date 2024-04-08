89e7b80d8597ea123c008181e0246279b46ba9b2
alexafer <alexafer@student.s19.be>
2024-04-08 16:25:46 +0200

    hey

--- srcs/ft_path/ft_cd.c
+++ srcs/ft_path/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 04:16:31 by alexafer          #+#    #+#             */
-/*   Updated: 2024/04/08 16:25:16 by alexafer         ###   ########.fr       */
+/*   Updated: 2024/04/08 16:25:31 by alexafer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -179,6 +179,7 @@ int	ft_execute(t_command *command, t_minishell *mini)
 		}
 		mini->stop = 1;
 		printf("Command not found.\n");
+		command->status = 1;
 	}
 	else
 	{
