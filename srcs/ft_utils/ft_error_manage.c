2c57dd44216539cce47a56a385a91bc928a86535
0x496D7065 <Imperiom@live.fr>
2024-04-14 19:29:29 +0200

    norms + fixed export exception

--- srcs/ft_utils/ft_error_manage.c
+++ srcs/ft_utils/ft_error_manage.c
@@ -6,7 +6,7 @@
 /*   By: alexafer <alexafer@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 20:27:20 by alexafer          #+#    #+#             */
-/*   Updated: 2024/04/02 17:33:29 by alexafer         ###   ########.fr       */
+/*   Updated: 2024/04/14 18:19:44 by lpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,31 @@ int	ft_error_msg(char *command)
 	ft_putstr_fd(command, 2);
 	ft_putstr_fd("\n", 2);
 	return (1);
+}
+
+void	ft_error_export(char *str)
+{
+	ft_putstr_fd("export: `", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd("':not a valid identifier\n", 2);
+}
+
+int	check_value(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && str[i] != '=')
+		i++;
+	i++;
+	while (str[i])
+	{
+		if (str[i] == ';' || str[i] == '(' || str[i] == ')')
+		{
+			ft_error_export(str);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
